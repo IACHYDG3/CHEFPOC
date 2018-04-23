07a21cea96d12441390d54871c43888676cdb58a
Hyd_ChefPOC <38035686+IACHYDG3@users.noreply.github.com>
2018-04-23 15:24:31 +0530

    Update runpowershell.rb
    changed the path of the script

--- cookbooks/starter/recipes/runpowershell.rb
+++ cookbooks/starter/recipes/runpowershell.rb
@@ -1,7 +1,7 @@
 powershell_script 'RunCode' do
   cwd 'C:\Users\Administrators'
   code <<-EOH
-  & C:\powershelltest\test.ps1
+  & C:\chef\cache\cookbooks\starter\files\default\WSUS.ps1
   EOH
   elevated True
 end
